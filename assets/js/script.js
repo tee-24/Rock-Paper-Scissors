6ed1af5bb4571a156b0bcea86f5e070d06367f93
Towbie Idowu <towbieidowu@gmail.com>
2024-03-18 23:49:59 +0000

    Add User Experience section to README

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,11 +24,5 @@ document.addEventListener("DOMContentLoaded", () => {
     span.addEventListener('click', () => {
         modal.style.display = 'none';
     })
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
     
 });
